9bbacac2c605a25d739e0d0476c97a2d36fe34b6
Gajraj Shekhawat <gaju2gajraj@gmail.com>
2022-07-21 15:24:34 +0530

    Draft

--- Tmetric-clone/Frontend/my-app/src/Components/ProjectBody.jsx
+++ Tmetric-clone/Frontend/my-app/src/Components/ProjectBody.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import styles from "../styles/projectbody.module.css";
+import { IoIosSearch } from "react-icons/io";
+import { TiArrowUp } from "react-icons/ti";
+import { TiArrowUnsorted } from "react-icons/ti";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { RiFolderOpenFill } from "react-icons/ri";
 
 const ProjectBody = () => {
+  let arr = [
+    { name: "masai", status: true, code: "lms1", team: "no team" },
+    { name: "masai1", status: false, code: "lms2", team: "no team" },
+    { name: "masai2", status: true, code: "lms3", team: "no team" },
+    { name: "masai3", status: false, code: "lms4", team: "no team" },
+    { name: "masai4", status: true, code: "lms5", team: "no team" },
+  ];
   return (
     <div>
       <div>
@@ -37,13 +49,64 @@ const ProjectBody = () => {
                 <button> +New Project</button>
               </div>
               <div>
-                <label className={styles.switch}>
-                  <input type="checkbox" checked />
-                  <span ></span>
-                </label>
+                <input
+                  type="checkbox"
+                  id="switch"
+                  className={styles.checkbox}
+                />
+                <label htmlFor="switch" className={styles.toggle}></label>
+              </div>
+            </div>
+            <div>
+              <div>
+                <h4>no. of project </h4>
+              </div>
+              <div>
+                <IoIosSearch />
+                <input type="text" />
               </div>
             </div>
           </div>
+          <div>
+            <div>
+              <input type="checkbox" name="" id="" />
+              <TiArrowUp />
+              <p>PROJECT</p>
+            </div>
+            <div>
+              <TiArrowUnsorted />
+              <p>CODE</p>
+            </div>
+            <div>
+              <p>TEAM</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <input type="checkbox" name="" id="" />
+              <MdKeyboardArrowDown />
+              <h5>No client(no. of project)</h5>
+            </div>
+          </div>
+          {arr.map((el, index) => (
+            <div key={index}>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <RiFolderOpenFill />
+                <p>{el.name}</p>
+                {el.status && <span>done</span>}
+              </div>
+              <div>
+                <p>{el.code}</p>
+              </div>
+              <div>
+                <p>No team</p>
+              </div>
+              <div>
+                <h2>...</h2>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
