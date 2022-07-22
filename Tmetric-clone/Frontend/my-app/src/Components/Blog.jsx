7c44c7762b9b858d3ca70b4386e22382cbed58df
Gajraj Shekhawat <gaju2gajraj@gmail.com>
2022-07-22 15:59:57 +0530

    Updated and routing part done

--- Tmetric-clone/Frontend/my-app/src/Components/Blog.jsx
+++ Tmetric-clone/Frontend/my-app/src/Components/Blog.jsx
@@ -1,81 +1,72 @@
 import React from "react";
 import style from "./Blog.module.css";
 import BlogCard from "./BlogCard";
-import Data from '../Data.json'
+import Data from "../Data.json";
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
 console.log(Data);
 const Blog = () => {
   return (
-    <div>
-      <div className={style.blognavbar}>
-        <img
-          src="https://blog.tmetric.com/content/images/size/w2000/2022/01/MicrosoftTeams-image--1-.png"
-          alt=""
-        />
-      </div>
-      {/* navbar completed /////////////////////////////////*/}
-
-      <div className={style.blogadpart}>
-        <div className={style.blogadpartimg}>
+    <>
+      <Navbar />
+      <div>
+        <div className={style.blognavbar}>
           <img
-            src="https://blog.tmetric.com/content/images/size/w1000/2022/06/productivity-banner-2.png"
+            src="https://blog.tmetric.com/content/images/size/w2000/2022/01/MicrosoftTeams-image--1-.png"
             alt=""
           />
         </div>
+        {/* navbar completed /////////////////////////////////*/}
 
-        <div>
-          <div className={style.blogadpartdetail}>
-            <h6>PRODUCTIVITY HACKS</h6>
-            <h1>
-              If You Want To Be <br /> Productive, Beware Of <br /> These 5
-              Distractions
-            </h1>
-
-            <p>
-              Distractions in the digital world spring up like <br />
-              mushrooms after the rain. The biggest is having
-              <br /> too many meetings. And there are four more that <br />
-              block a productive and balanced life. If you know <br />
-              them, you are better equipped for dealing with them.
-            </p>
+        <div className={style.blogadpart}>
+          <div className={style.blogadpartimg}>
+            <img
+              src="https://blog.tmetric.com/content/images/size/w1000/2022/06/productivity-banner-2.png"
+              alt=""
+            />
           </div>
 
-          <div className={style.blogadpartprofile}>
-            <div className={style.blogadpartprofileimg}>
-              <img
-                src="https://blog.tmetric.com/content/images/size/w100/2021/11/-----1.png"
-                alt=""
-              />
+          <div>
+            <div className={style.blogadpartdetail}>
+              <h6>PRODUCTIVITY HACKS</h6>
+              <h1>
+                If You Want To Be <br /> Productive, Beware Of <br /> These 5
+                Distractions
+              </h1>
+
+              <p>
+                Distractions in the digital world spring up like <br />
+                mushrooms after the rain. The biggest is having
+                <br /> too many meetings. And there are four more that <br />
+                block a productive and balanced life. If you know <br />
+                them, you are better equipped for dealing with them.
+              </p>
             </div>
-            <div>
-              <h5>Alla Chernets</h5>
+
+            <div className={style.blogadpartprofile}>
+              <div className={style.blogadpartprofileimg}>
+                <img
+                  src="https://blog.tmetric.com/content/images/size/w100/2021/11/-----1.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h5>Alla Chernets</h5>
+              </div>
             </div>
           </div>
         </div>
+
+        {/* blog deatail ends///////////////////////////////////// */}
+
+        <div className={style.blogadpartallprofilemap}>
+          {Data.blog.map((item) => {
+            return <BlogCard key={item.id} item={item} />;
+          })}
+        </div>
       </div>
-
-{/* blog deatail ends///////////////////////////////////// */}
-
-
-
-<div className={style.blogadpartallprofilemap}>
-
-{
-    Data.blog.map((item)=>{
-        return <BlogCard key={item.id}  item={item}/>
-    })
-}
-
-
-</div>
-
-
-
-
-
-
-
-
-    </div>
+      <Footer />
+    </>
   );
 };
 
--- Tmetric-clone/Frontend/my-app/src/Components/Navbar.jsx
+++ Tmetric-clone/Frontend/my-app/src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { BiChevronDown } from 'react-icons/bi'
 import { BsGlobe2 } from 'react-icons/bs'
 import { DropdownWhyTMatric } from './DropdownWhyTMatric';
+import {useNavigate} from 'react-router-dom'
 
 import './Navbar.css'
 import { DropdownApp } from './DropdownApp&Integeration';
@@ -17,6 +18,7 @@ export const Navbar = () => {
         setWhyTMetric(false)
         setApps(!appsDropdown)
     }
+    const navigate = useNavigate()
     return (
         <div className='NavbarMainDiv'>
             <div className='NavbarImage'>
@@ -26,11 +28,13 @@ export const Navbar = () => {
             <div className={`NavbarPageName ${whyTMetricDropdown && 'dropdownSelected'}`}><span onClick={WhyTMatricClick}>Why TMetric</span><BiChevronDown className='NavbarDownArrow' /></div>
             <div className={`NavbarPageName ${appsDropdown && 'dropdownSelected'}`} ><span onClick={AppClick}>Apps & Integrations</span><BiChevronDown className='NavbarDownArrow' /></div>
             <div className='NavbarPageName'><Link to={'/'}>Pricing</Link></div>
-            <div className='NavbarPageName'><Link to={'/'}>Support</Link></div>
-            <div className='NavbarPageName'><Link to={'/'}>Blog</Link> </div>
+            <div className='NavbarPageName'><Link to={'/support'}>Support</Link></div>
+            <div className='NavbarPageName'><Link to={'/blog'}>Blog</Link> </div>
             <div className="NavbarLoginSignup">
-                <Link to="/">Log In</Link>
-                <button>Sign Up</button>
+                <Link to="/signin">Log In</Link>
+                <button onClick={()=>{
+                    navigate("/signup")
+                }}>Sign Up</button>
             </div>
             {whyTMetricDropdown && <DropdownWhyTMatric />}
             {appsDropdown && <DropdownApp />}
