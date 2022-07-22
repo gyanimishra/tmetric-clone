ae00c5eb5cb86a25c93630e4a80f3534190c3794
Gajraj Shekhawat <gaju2gajraj@gmail.com>
2022-07-22 22:37:02 +0530

    Added

--- Tmetric-clone/Frontend/my-app/src/Components/NewProjectForm.jsx
+++ Tmetric-clone/Frontend/my-app/src/Components/NewProjectForm.jsx
@@ -4,59 +4,136 @@ import { FiArrowLeft } from "react-icons/fi";
 
 const NewProjectForm = () => {
   const ref = useRef([]);
+
   return (
-    <div>
-      <div>
-        <h1>
-          <FiArrowLeft />
-          NewProject
-        </h1>
-        <div>
-          <div>
-            <button>General</button>
-            <button>Billing</button>
-            <button>Budget</button>
-            <button>Work types</button>
-            <button>Team</button>
+    <div className={styles.main_container_form}>
+      <div className={styles.form_top_div}>
+        
+          <FiArrowLeft  fontSize={"30px"}/>
+        
+        <h1>New Project</h1>
+      </div>
+      <div className={styles.form_content_div}>
+        <div className={styles.form_content_btn_div}>
+          <button>General</button>
+          <button>Billing</button>
+          <button>Budget</button>
+          <button>Work types</button>
+          <button>Team</button>
+        </div>
+        <div className={styles.form_content_data_div}>
+          <div className={styles.form_content_data_general}>
+            <h2>
+              General <span> Active</span>
+            </h2>
           </div>
           <div>
-            <div>
-              <h1>
-                General <span>Active</span>
-              </h1>
-            </div>
-            <div>
-              <form>
+            <form>
+              <div className={styles.form_content_data_div1}>
                 <div>
+                  <p>Project name</p>
+                  <input className= {styles.projectnameInput}type="text" placeholder="Enter project name" />
+                </div>
+                <div>
+                  <p>Project Code</p>
+                  <input type="text" placeholder="PR-01" />
+                </div>
+              </div>
+
+              <div className={styles.form_content_data_div1}>
+                <div>
+                  <p>Client</p>
+                  <select>
+                    <option value=""> Select Client</option>
+                  </select>
+                </div>
+                <div>
+                  <input type="file" />
+                </div>
+              </div>
+
+              <div>
+                <p>Notes</p>
+                <textarea placeholder="Additional information on the project"></textarea>
+              </div>
+
+              <div>
+                <div>
+                  <h2>Billing</h2>
                   <div>
-                    <p>Project name</p>
-                    <input type="text" placeholder="Enter project name" />
-                  </div>
-                  <div>
-                    <p>Project Code</p>
-                    <input type="text" placeholder="PR-01" />
+                    <input
+                      type="checkbox"
+                      id="switch"
+                      className={styles.checkbox}
+                    />
+                    <label htmlFor="switch" className={styles.toggle}></label>
                   </div>
                 </div>
                 <div>
+                  <p>
+                    To set uo billing options for the project,mark it as
+                    billable
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <h2>Budget</h2>
                   <div>
-                    <p>Client</p>
-                    <select>
-                      <option value=""> Select Client</option>
-                    </select>
-                  </div>
-                  <div>
-                    <input type="file" />
+                    <input
+                      type="checkbox"
+                      id="switch"
+                      className={styles.checkbox}
+                    />
+                    <label htmlFor="switch" className={styles.toggle}></label>
                   </div>
                 </div>
                 <div>
-                  <p>Notes</p>
-                  <textarea placeholder="Additional information on the project"></textarea>
+                  <p>
+                    To add a budget to the project,you need to enable the
+                    section
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <h2>Work types</h2>
                 </div>
                 <div>
-                    
+                  <p>
+                    There are no work types in the workspace, You should{" "}
+                    <span>create a work type.</span>
+                  </p>
                 </div>
-              </form>
-            </div>
+              </div>
+
+              <div>
+                <div>
+                  <h2>Team</h2>
+                </div>
+                <div>
+                  <button>+ Add members to project team</button>
+                </div>
+              </div>
+
+              <div>
+                <p>
+                  It's a public project. Time can be recorded by every member.
+                  All workspace members will see the project in their reports.
+                  Add team members to restrict project visibility.
+                </p>
+              </div>
+
+              <div>
+                <p>Press Ctrl+Enter to save changes</p>
+                <div>
+                  <button type="submit">Save</button>
+                  <button>Cancel</button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
--- Tmetric-clone/Frontend/my-app/src/routes/AllRoutes.jsx
+++ Tmetric-clone/Frontend/my-app/src/routes/AllRoutes.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
 import { HomePage } from "../Components/HomePage";
 import Support from "../Components/Support";
 import Blog from "../Components/Blog";
 import Signin from "../Components/Signin";
 import Signup from "../Components/SignUp";
+import ProjectBody from "../Components/ProjectBody";
+import NewProjectForm from "../Components/NewProjectForm";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/project" element={<Dashboard />} />
+      <Route path="/project" element={<ProjectBody />} />
+      <Route path="/project/new" element={<NewProjectForm />} />
       <Route path="/support" element={<Support />} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/signin" element={<Signin />} />
