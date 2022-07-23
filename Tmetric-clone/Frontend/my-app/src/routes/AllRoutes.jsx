19c45ba7e2a9cc58cb66c6f63e995031feda3ad5
Divya Bairwa <98870450+DivyaBairwa@users.noreply.github.com>
2022-07-23 22:13:36 +0530

    sidebar

--- Tmetric-clone/Frontend/my-app/src/routes/AllRoutes.jsx
+++ Tmetric-clone/Frontend/my-app/src/routes/AllRoutes.jsx
@@ -3,20 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../Components/HomePage";
 import Support from "../Components/Support";
 import Blog from "../Components/Blog";
+// import Signin from "../Components/Signin";
 import Signup from "../Components/SignUp";
-import ProjectBody from "../Components/ProjectBody";
 import NewProjectForm from "../Components/NewProjectForm";
-import Price from "../Components/Price"
+import Dashboard from "../pages/Dashboard";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/project" element={<ProjectBody />} />
+      <Route path="/project" element={<Dashboard />} />
       <Route path="/project/new" element={<NewProjectForm />} />
       <Route path="/support" element={<Support />} />
-      <Route path="/pricing" element={<Price />} />
       <Route path="/blog" element={<Blog />} />
+      {/* <Route path="/signin" element={<Signin />} /> */}
       <Route path="/signup" element={<Signup />} />
     </Routes>
   );
